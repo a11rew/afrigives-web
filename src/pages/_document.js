c18ef3cc3c6d98d338fc43a7cf16d4fb9b0461d4
Andrew Glago <andrewglago1@gmail.com>
2022-03-11 00:46:44 +0000

    Setup header

---
+++ src/pages/_document.js
@@ -0,0 +1,22 @@
+import Document, { Html, Head, Main, NextScript } from "next/document";
+
+export default class MyDocument extends Document {
+  render() {
+    return (
+      <Html>
+        <Head>
+          <link
+            rel="stylesheet"
+            media="screen"
+            href="https://fontlibrary.org//face/open-sauce-one"
+            type="text/css"
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
