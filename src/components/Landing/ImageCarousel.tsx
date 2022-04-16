f437cfa77a142dbabe8e9aac0e7a647bbc1a0fc4
Andrew Glago <andrewglago1@gmail.com>
2022-04-16 13:51:35 +0000

    Add animated carousel

--- src/components/Landing/ImageCarousel.tsx
+++ src/components/Landing/ImageCarousel.tsx
@@ -1,6 +1,8 @@
 /* eslint-disable @next/next/no-img-element */
-import Image from "next/image";
 import React from "react";
+import Carousel from "react-multi-carousel";
+import Image from "next/image";
+import "react-multi-carousel/lib/styles.css";
 
 import StockGirls from "../../assets/images/girls.jpg";
 import StockKids from "../../assets/images/kids.jpg";
@@ -9,36 +11,95 @@ import StockWomen from "../../assets/images/women.jpg";
 
 const ImageCarousel = () => {
   return (
-    <div className="flex gap-5 overflow-x-auto scrollbar-hide">
+    <Carousel
+      additionalTransfrom={0}
+      autoPlay
+      autoPlaySpeed={300}
+      arrows={false}
+      centerMode={false}
+      className=""
+      containerClass=""
+      customTransition="all 30s linear"
+      focusOnSelect={false}
+      infinite
+      itemClass="mx-2"
+      keyBoardControl
+      minimumTouchDrag={80}
+      renderButtonGroupOutside={false}
+      renderDotsOutside={false}
+      responsive={{
+        desktop: {
+          breakpoint: {
+            max: 3000,
+            min: 1024,
+          },
+          items: 4,
+          partialVisibilityGutter: 40,
+        },
+        mobile: {
+          breakpoint: {
+            max: 464,
+            min: 0,
+          },
+          items: 1,
+          partialVisibilityGutter: 30,
+        },
+        tablet: {
+          breakpoint: {
+            max: 1024,
+            min: 464,
+          },
+          items: 2,
+          partialVisibilityGutter: 30,
+        },
+      }}
+      showDots={false}
+      sliderClass=""
+      slidesToSlide={2}
+      swipeable
+      transitionDuration={30000}
+    >
       <div>
-        <img
-          className="min-w-[60vw] sm:min-w-[27.5vw] h-[392px] object-cover"
-          src={StockGirls.src}
-          alt="Image of smiling kids"
-        />
+        <div className="min-w-[60vw] sm:min-w-[27.5vw] h-[392px] px-2 overflow-hidden">
+          <Image
+            layout="fill"
+            objectFit="cover"
+            src={StockGirls}
+            alt="Image of smiling kids"
+          />
+        </div>
       </div>
       <div>
-        <img
-          className="min-w-[60vw] sm:min-w-[27.5vw] h-[392px] object-cover"
-          src={StockKids.src}
-          alt="Image of school kids in uniform"
-        />
+        <div className="min-w-[60vw] sm:min-w-[27.5vw] h-[392px] px-2 overflow-hidden">
+          <Image
+            layout="fill"
+            objectFit="cover"
+            src={StockKids}
+            alt="Image of school kids in uniform"
+          />
+        </div>
       </div>
       <div>
-        <img
-          className="min-w-[60vw] sm:min-w-[27.5vw] h-[392px] object-cover"
-          src={StockTrad.src}
-          alt="Image of celebrating women"
-        />
+        <div className="min-w-[60vw] sm:min-w-[27.5vw] h-[392px] px-2 overflow-hidden">
+          <Image
+            layout="fill"
+            objectFit="cover"
+            src={StockTrad}
+            alt="Image of celebrating women"
+          />
+        </div>
       </div>
       <div>
-        <img
-          className="min-w-[60vw] sm:min-w-[27.5vw] h-[392px] object-cover"
-          src={StockWomen.src}
-          alt="Image of women in traditional garb"
-        />
+        <div className="min-w-[60vw] sm:min-w-[27.5vw] h-[392px] px-2 overflow-hidden">
+          <Image
+            layout="fill"
+            objectFit="cover"
+            src={StockWomen}
+            alt="Image of women in traditional garb"
+          />
+        </div>
       </div>
-    </div>
+    </Carousel>
   );
 };
 
